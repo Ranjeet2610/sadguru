94e22ef336fab7ee116490218b76632f29082c5f
Ranjeet <ranjeetsuneja26@gmail.com>
2025-02-23 16:24:00 +0530

    add changes

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -152,6 +152,12 @@ export const Contact = (props) => {
       </div>
       <div id="footer">
         <div className="container text-center">
+        <p>
+        Disclaimer:- This Website is only for 18+ users. If you are from Telangana, Orissa, Assam, Sikkim,Andhra Pradesh and Nagaland Please leave the website immediately. Be aware of fraudsters, we only deal via WhatsApp. We Only Promote Fantasy Sports. No Real Money Involvement.{" "}
+            {/* <a href="http://www.templatewire.com" rel="nofollow">
+              TemplateWire
+            </a> */}
+          </p>
           <p>
           Copyright © 2025 Sadguru11 - All Rights Reserved.{" "}
             {/* <a href="http://www.templatewire.com" rel="nofollow">
