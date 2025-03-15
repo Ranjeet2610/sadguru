90d99e55c6478045d2cb78c5a10753db07f1702d
Ranjeet <ranjeetsuneja26@gmail.com>
2025-03-16 00:08:04 +0530

    changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Header = (props) => {
   return (
     <header id="header">
+      <a href="https://wa.link/6ppkwz" target="_blank" rel="noopener noreferrer"></a>
       <div className="intro">
+      <a href="https://wa.link/6ppkwz" target="_blank" rel="noopener noreferrer"></a>
         <div className="overlay">
           <div className="container">
             <div className="row">
@@ -13,12 +15,7 @@ export const Header = (props) => {
                   <span></span>
                 </h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                <a
-                  href="https://wa.link/6ppkwz"
-                  className="btn btn-custom btn-lg page-scroll"
-                >
-                  get ur id here
-                </a>{" "}
+                {/* <a href="https://wa.link/6ppkwz" className="btn btn-custom btn-lg page-scroll" > get ur id here </a>{" "} */}
               </div>
             </div>
           </div>
