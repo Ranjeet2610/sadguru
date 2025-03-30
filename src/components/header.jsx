c3b740ddcb5093a0257403e1ce00f96543bbda70
Ranjeet <ranjeetsuneja26@gmail.com>
2025-03-30 20:06:57 +0530

    add number

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
-      <a href="https://wa.link/6ppkwz" className="background-link" target="_blank" rel="noopener noreferrer"></a>
+      <a href="https://wa.link/ugbkx3" className="background-link" target="_blank" rel="noopener noreferrer"></a>
         <div className="overlay">
           <div className="container">
             <div className="row">
@@ -14,7 +14,7 @@ export const Header = (props) => {
                   <span></span>
                 </h1> */}
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                {/* <a href="https://wa.link/6ppkwz" className="btn btn-custom btn-lg page-scroll" > get ur id here </a>{" "} */}
+                {/* <a href="https://wa.link/ugbkx3" className="btn btn-custom btn-lg page-scroll" > get ur id here </a>{" "} */}
               </div>
             </div>
           </div>
