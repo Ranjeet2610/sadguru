71e234788c4bdb2b5edaf4a22afca18ef15feaee
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-06-03 00:15:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,8 @@ const App = () => {
       {/* <Navigation /> */}
       <Header data={landingPageData.Header} />
       {/* <Features data={landingPageData.Features} /> */}
-      {/* <About data={landingPageData.About} /> */}
-      {/* <Services data={landingPageData.Services} /> */}
+      <About data={landingPageData.About} />
+      <Services data={landingPageData.Services} />
       {/* <Gallery data={landingPageData.Gallery} /> */}
       {/* <Testimonials data={landingPageData.Testimonials} /> */}
       {/* <Team data={landingPageData.Team} /> */}
