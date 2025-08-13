241f2a07bb139e94181e4191019f452745894b20
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-14 00:55:25 +0530

    gtm

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 
 export const Header = (props) => {
+  const handleIntroClick = () => {
+    if (window.gtag) {
+      window.gtag('event', 'conversion', {
+        send_to: 'AW-17453639828/cTbhCfIH64UbEJTrXyJB'
+      });
+    }
+  };
+
   return (
     <header id="header">
       <div className="intro">
-      <a href="https://wa.link/0fdcfh" className="background-link" target="_blank" rel="noopener noreferrer"></a>
+        <a
+          href="https://wa.link/0fdcfh"
+          className="background-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={handleIntroClick} // Fire conversion when clicked
+        ></a>
         <div className="overlay">
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                {/* <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1> */}
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
-                {/* <a href="https://wa.me/+919582844364" className="btn btn-custom btn-lg page-scroll" > get ur id here </a>{" "} */}
               </div>
             </div>
           </div>
