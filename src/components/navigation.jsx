c938034e914c044581ebe8f9f6b851ccfdb6d06d
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-13 13:47:31 +0530

    changes5

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <a className="navbar-brand page-scroll" href="https://wa.link/q1thbi">
-            booksdads2.shop
+            booksshop.online
           </a>{" "}
         </div>
 
