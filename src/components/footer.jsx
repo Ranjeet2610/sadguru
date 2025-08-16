bf7d8435695bb08f5ebd3cfdedf177bac9023bfb
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-16 20:01:15 +0530

    changes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div style={{ padding: "20px", fontSize: "14px", textAlign: "center", background: "#f8f8f8" }}>
-      Disclaimer:- This Website is only for 18+ users. If you are from Telangana, Orissa, Assam, Sikkim,Andhra Pradesh and Nagaland Please leave the website immediately. Be aware of fraudsters, we only deal via WhatsApp. We Only Promote Fantasy Sports. No Real Money Involvement.
+      Disclaimer:- This Website is only for 18+ users. If you are from Telangana, Odisha, Assam, Sikkim,Andhra Pradesh and Nagaland Please leave the website immediately. Be aware of fraudsters, we only deal via WhatsApp. We Only Promote Fantasy Sports. No Real Money Involvement.
       <br /><br />
       <a
         href="https://wa.link/y0c3ps"
