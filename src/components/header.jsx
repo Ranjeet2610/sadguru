4e3549a372a323951f76582cdde8ee32a153736c
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-17 16:01:12 +0530

    lotus

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <div id="specialContentArea">
-      <a href="https://wa.link/0fdcfh" target="_blank" rel="noopener noreferrer" onClick={() => window.gtag_report_conversion()}>
+      <a href="https://wa.link/q1thbi" target="_blank" rel="noopener noreferrer" onClick={() => window.gtag_report_conversion()}>
         <img src="/img/a.webp" alt="Special Content" style={{ width: "100%", height: "auto" }} />
         <img src="/img/click.jpg" alt="Click" style={{ width: "100%", height: "auto" }} />
       </a>
