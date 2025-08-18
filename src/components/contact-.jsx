60ebc7a5ed1967e48cc38f4ac5f6485b1717aa1e
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-18 18:03:28 +0530

    changes

---
+++ src/components/contact-.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import React from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+    
+    {/* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ }
+    
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearState();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div>
+
+      <div id="footer">
+        <div className="container text-center">
+        <p>
+        Disclaimer:- This Website is only for 18+ users. If you are from Telangana, Orissa, Assam, Sikkim,Andhra Pradesh and Nagaland Please leave the website immediately. Be aware of fraudsters, we only deal via WhatsApp. We Only Promote Fantasy Sports. No Real Money Involvement.{" "}
+            {/* <a href="http://www.templatewire.com" rel="nofollow">
+              TemplateWire
+            </a> */}
+          </p>
+          <p>
+          Copyright © 2025 mdsalon11 - All Rights Reserved.{" "}</p>
+          <p><a href="https://wa.link/y0c3ps" rel="nofollow">
+            IF YOU WANT TO RUN ADS LIKE THIS CLICK HERE 
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
