3d80c7867b04191e9c93e64c6b7eb8ec0cfb565a
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-18 16:47:36 +0530

    lotus

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,14 +6,14 @@ const Footer = () => {
     <div style={{ padding: "20px", fontSize: "14px", textAlign: "center", background: "#f8f8f8" }}>
       Disclaimer:- This Website is only for 18+ users. If you are from Telangana, Odisha, Assam, Sikkim,Andhra Pradesh and Nagaland Please leave the website immediately. Be aware of fraudsters, we only deal via WhatsApp. We Only Promote Fantasy Sports. No Real Money Involvement.
       <br /><br />
-      <a
+      {/* <a
         href="https://wa.link/y0c3ps"
         target="_blank"
         rel="noopener noreferrer"
         onClick={() => window.gtag_report_conversion()}
       >
         <img src="/img/1500x800.png" alt="Click" style={{ width: "100%", height: "auto" }} />
-      </a>
+      </a> */}
     </div>
   );
 };
