107530fbab2c6810b0f0f815c882fde39e390472
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-19 23:46:17 +0530

    change code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
   return (
     <div id="specialContentArea">
-      <a  href="https://wa.link/0fdcfh" target="_blank" rel="noopener noreferrer" onClick={() => {window.gtag('event', 'conversion', { send_to: 'AW-17453639828/cfnPCND5zokbEJTRxYJB',  // Replace XXXXXXX with the conversion label
+      <a  href="https://wa.link/0fdcfh" target="_blank" rel="noopener noreferrer" onClick={() => {window.gtag('event', 'conversion', { send_to: 'AW-17453639828/-i0xCK7Xz4kbEJTRxYJB',  // Replace XXXXXXX with the conversion label
     });
   }}
 >
