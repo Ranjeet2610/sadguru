3308158f5c1a63b5a9baa3629cc496a9c3606597
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-19 23:59:19 +0530

    changes

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,10 +4,18 @@ import React from "react";
 const Header = () => {
   return (
     <div id="specialContentArea">
-      <a  href="https://wa.link/0fdcfh" target="_blank" rel="noopener noreferrer" onClick={() => {window.gtag('event', 'conversion', { send_to: 'AW-17453639828/-i0xCK7Xz4kbEJTRxYJB',  // Replace XXXXXXX with the conversion label
+      
+  <a
+  href="https://wa.link/0fdcfh"
+  target="_blank"
+  rel="noopener noreferrer"
+  onClick={() => {
+    window.gtag('event', 'conversion', {
+      send_to: 'AW-17453639828/-i0xCK7Xz4kbEJTRxYJB'
     });
   }}
 >
+
         <img src="/img/a.webp" alt="Special Content" style={{ width: "100%", height: "auto" }} />
         <img src="/img/click.jpg" alt="Click" style={{ width: "100%", height: "auto" }} />
       </a>
