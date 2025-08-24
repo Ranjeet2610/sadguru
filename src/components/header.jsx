e9516caa8b5bdda978279c3045e3a6ec95df5b70
Ranjeet2610 <ranjeetsuneja26@gmail.com>
2025-08-25 01:47:11 +0530

    fix image

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
   }}
 >
 
-        <img src="/a.webp" alt="Special Content" style={{ width: "100%", height: "auto" }} />
+        <img src="/a.jpg" alt="Special Content" style={{ width: "100%", height: "auto" }} />
         <img src="/click.jpg" alt="Click" style={{ width: "100%", height: "auto" }} />
       </a>
     </div>
